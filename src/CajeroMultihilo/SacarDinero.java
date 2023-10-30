ca4f26bf52e093147ee0f5e9ea2e4bf36476e83d
fuentessergio <129279129+fuentessergio@users.noreply.github.com>
2023-10-30 11:41:04 +0100

    commit and push

---
+++ src/CajeroMultihilo/SacarDinero.java
@@ -0,0 +1,17 @@
+package CajeroMultihilo;
+
+public class SacarDinero extends Thread{
+        private Cuenta c;
+        String n;
+
+        public SacarDinero(String n, Cuenta c) {
+            super(n);
+            this.c=c;
+        }
+
+        public void run() {
+            for (int x=1; x<=4;x++) {
+                c.RetirarDinero(10, getName());
+            }
+        }
+}
