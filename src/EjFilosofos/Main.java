67ed3468bfa5c2dba6df2ed5dcf0dca0dbaf190f
fuentessergio <129279129+fuentessergio@users.noreply.github.com>
2023-11-07 10:25:37 +0100

    filosofos OK

--- src/EjFilosofos/Main.java
+++ src/EjFilosofos/Main.java
@@ -2,11 +2,25 @@ package EjFilosofos;
 
 public class Main {
     public static void main(String[] args) {
-        Filosofo f1 = new Filosofo("Sócrates");
-        Filosofo f2 = new Filosofo("Aristóteles");
-        Filosofo f3 = new Filosofo("Platon");
-        Filosofo f4 = new Filosofo("Ortega y Gasset");
-        Filosofo f5 = new Filosofo("Descartes");
+        int numFilosofos = 5;
+        Palillos [] palillos = new Palillos[numFilosofos];
+        Filosofo [] filosofos = new Filosofo[numFilosofos];
+
+        for (int i = 0; i < numFilosofos; i++) {
+            palillos[i] = new Palillos();
+        }
+
+        for (int i = 0; i < numFilosofos; i++) {
+            filosofos[i] = new Filosofo(palillos[i], palillos[(i+1) % numFilosofos]);
+            filosofos[i].setNombre(i);
+            filosofos[i].start();// palillos[i] palilloIzq y palillos[...] palilloDcha
+        }
+        try{
+            Thread.sleep(1000); // que dure 10 seg
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 
 }
--- src/EjFilosofos/Palillos.java
+++ src/EjFilosofos/Palillos.java
@@ -1,7 +1,18 @@
 package EjFilosofos;
 
 public class Palillos{
+    private boolean estaLibre=true;
 
+    public synchronized void cogerPalillo () throws InterruptedException {
+        while(!estaLibre){
+            wait();
+        }
+        estaLibre = false;
+    }
 
+    public synchronized void dejarPalillo () throws InterruptedException{
+        estaLibre=true;
+        notify();
+        }
+    }
 
-}
