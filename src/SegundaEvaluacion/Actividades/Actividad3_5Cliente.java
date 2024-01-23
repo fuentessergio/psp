c264a012d0e09dbbe8e4e173571be1674c81deb0
fuentessergio <129279129+fuentessergio@users.noreply.github.com>
2024-01-23 10:21:55 +0100

    3.5

---
+++ src/SegundaEvaluacion/Actividades/Actividad3_5Cliente.java
@@ -0,0 +1,44 @@
+package SegundaEvaluacion.Actividades;
+
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
+public class Actividad3_5Cliente {
+    public static void main(String[] args) throws Exception {
+        String destino = "localhost";
+        System.out.println("Variable destino: "+destino);
+        int port = 5051; //puerto al que envio el datagrama
+
+        try{
+            DatagramSocket clientSocket = new DatagramSocket(1024);
+            Scanner scanner = new Scanner(System.in);
+
+            while(true){
+                System.out.println("Introduce una cadena o '*' para salir");
+                String mensaje = scanner.nextLine();
+
+                byte [] cadenaEnviada = mensaje.getBytes(); // obtener bytes
+                DatagramPacket envio = new DatagramPacket(cadenaEnviada, cadenaEnviada.length, InetAddress.getByName(destino),port);
+                clientSocket.send(envio);
+
+                byte [] cadenaRecibida = new byte[1024];
+                DatagramPacket recibo = new DatagramPacket(cadenaRecibida, cadenaRecibida.length);
+                clientSocket.receive(recibo);
+
+                String respuestaServidor = new String(recibo.getData());
+                System.out.println("Respuesta del servidor: " + respuestaServidor);
+
+                if(mensaje.trim().equals("*")){
+                    System.out.println("Cliente cerrado");
+                    break;
+                }
+            clientSocket.close();
+            }
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/SegundaEvaluacion/Actividades/Actividad3_5Servidor.java
@@ -0,0 +1,60 @@
+package SegundaEvaluacion.Actividades;
+
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
+/**
+ * El servidor devolverá la cadena que le envíe el cliente en mayúsculas, tantas veces
+ * como el cliente le envíe mensajes. El servidor terminará su ejecución cuando la
+ * cadena de caracteres que reciba del cliente sea un asterisco.
+ * El cliente, en un bucle infinito, pedirá al usuario final que introduzca una cadena de
+ * caracteres que posteriormente enviará al servidor y mostrará por pantalla la
+ * respuesta del servidor. Cuando el usuario final introduzca un asterisco, el cliente se
+ * lo enviará al servidor y finalizará su ejecución.
+ * Después de los dos primeros puntos, añade en el servidor un tiempo de espera de
+ * 50000 milisegundos para hacer que el método receive() no esté esperando
+ * eternamente. Controla la excepción y cierra la conexión correctamente. Pruébalo.
+ * Prueba también a ejecutar varios programas clientes a la vez.
+ */
+public class Actividad3_5Servidor {
+    public static void main(String[] args) {
+        int port = 5051;
+
+        try{
+            DatagramSocket datagramSocket = new DatagramSocket(port);
+            System.out.println("Esperando Datagrama .......... ");
+
+            while(true) {
+                // Crear un paquete para recibir datos del cliente
+                byte[] bufer = new byte[1024];
+                DatagramPacket recibo = new DatagramPacket(bufer, bufer.length);
+
+                // Esperar a recibir datos del cliente
+                datagramSocket.receive(recibo);
+                String mensaje = new String(recibo.getData());
+                System.out.println("Mensaje recibido del cliente: " + mensaje);
+                String mensajeM = mensaje.toUpperCase();
+
+                // obtener direccion IP y puerto del cliente
+                InetAddress clientAddress = recibo.getAddress();
+                int clientPort = recibo.getPort();
+
+                // enviar mensaje en mayusculas
+                byte[] salida = mensajeM.getBytes();
+                DatagramPacket envio = new DatagramPacket(salida, salida.length, clientAddress, clientPort);
+
+                datagramSocket.send(envio);
+
+                if (mensaje.trim().equals("*")) {
+                    System.out.println("Servidor cerrado");
+                    break;
+                }
+            }
+
+            datagramSocket.close();
+        }catch (Exception e){
+            throw new RuntimeException("ERROR " + e.getMessage());
+        }
+    }
+}
